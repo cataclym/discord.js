e07bbc2b1fba2f749e9d05f39610b490a7f24a2e
Shubham Parihar <shubhamparihar391@gmail.com>
2021-09-22 10:19:30 +0530

    feat(Role): add check for unicodeEmoji in #equals

--- src/structures/Role.js
+++ src/structures/Role.js
@@ -403,7 +403,8 @@ class Role extends Base {
       this.position === role.position &&
       this.permissions.bitfield === role.permissions.bitfield &&
       this.managed === role.managed &&
-      this.icon === role.icon
+      this.icon === role.icon &&
+      this.unicodeEmoji === role.unicodeEmoji
     );
   }
 
